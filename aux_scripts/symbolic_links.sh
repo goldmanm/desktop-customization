380ec75dce5954564946b458df45b65f5e79c4c4
mark goldman <goldmanm@mit.edu>
2017-01-27 10:19:01 -0500

    added thunderbird sync to symbolic links

--- aux_scripts/symbolic_links.sh
+++ aux_scripts/symbolic_links.sh
@@ -3,3 +3,5 @@ ln -s ~/Dropbox\ \(MIT\)/.bash_aliases ~/.bash_aliases
 ln -s ~/Dropbox\ \(MIT\)/personal/writings ~/workspace/website/_drafts
 
 ln -s ~/Dropbox\ \(MIT\)/Research ~/research
+
+ln -s ~/Dropbox\ \(MIT\)/scripts/thunderbird_files/ ~/.thunderbird
