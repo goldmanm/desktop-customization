71c42119c7284edfa6ee516536e6d1d92eb32646
MarkGoldman <goldmanm@mit.edu>
2020-09-09 21:50:14 -0400

    update to include bluetooth & make and better formatting

--- install_programs.sh
+++ install_programs.sh
@@ -4,12 +4,12 @@ sudo add-apt-repository -y ppa:smathot/cogscinl
 sudo apt-get update
 
 sudo apt-get upgrade -y
-
-#zotero
+# add repositories
+# for zotero
 sudo add-apt-repository -y ppa:smathot/cogscinl
+sudo apt-get update
 
 #general
-sudo apt-get install -y git
 sudo apt-get install -y texlive-full pandoc texstudio okular
 sudo apt-get install -y thunderbird gedit firefox redshift-gtk
 sudo apt-get install -y gtk-recordmydesktop shutter
@@ -17,9 +17,18 @@ sudo apt-get install -y nautilus-dropbox gparted
 sudo apt-get install -y sed
 sudo apt-get install -y pinta inkscape
 sudo apt-get install -y calibre audacity
+# disk file space viewer
 sudo apt-get install -y baobab
+# use locate method
 sudo apt-get install -y mlocate
+# capture screenshots
 sudo apt-get install -y flameshot
+# get bluetooth working
+sudo apt-get install -y bluetooth bluez bluez-tools rfkill blueman
+# control sound
+sudo apt-get install -y pavucontrol pasystray
+
+
 
 # 4 encryption
 sudo apt-get install -y encfs
@@ -30,12 +39,13 @@ sudo apt-get install -y openconnect
 # sudo apt-get install -y openssh-server
 sudo apt-get install -y openssh-client
 sudo apt-get install -y gcc gfortran 
+sudo apt-get install -y make
 
 ##########for work
 sudo apt-get install -y libreoffice
 sudo apt-get install -y zotero-standalone
 #auto-setup site-manager
-sudo apt-get install -y filezilla			
+sudo apt-get install -y filezilla
 sudo apt-get install -y avogardro
 
 ##########for markdown
